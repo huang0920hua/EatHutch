5a2c7afe81caad21ca3768b0db08fb119b661c94
huangzh <hz824581849@qq.com>
2018-07-27 15:22:17 +0800

    后台工程创建

---
+++ EatHutchSolution/EatHutchWebAPI/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace EatHutchWebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        void ConfigureApi(HttpConfiguration config)
+        {
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            ConfigureApi(GlobalConfiguration.Configuration);
+        }
+    }
+}
