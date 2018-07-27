62655456356b284bbcc9ffbbf74915547ea1ec1a
huangzh <hz824581849@qq.com>
2018-07-27 17:52:42 +0800

    update

---
+++ EatHutchSolution/EatHutchWebAPI/Controllers/FoodTypesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EatHutchWebAPI.Controllers
+{
+    public class FoodTypesController : ApiController
+    {
+        public string GetFoodTypes()
+        {
+            return "dddddddd";
+        }
+    }
+}
